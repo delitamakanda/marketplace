cd13ad3c8a1df2e01e1286d0ef5e6db51193d8c3
Délita Makanda <delita.makanda@gmail.com>
2021-01-04 00:50:00 +0100

    fix tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  purge: ['./**/*.html'],
+  future: {
+    purgeLayersByDefault: true
+  },
+  purge: [
+    './account/templates/**/*.html',
+    './cart/templates/**/*.html',
+    './catalog/templates/**/*.html',
+    './orders/templates/**/*.html',
+    './payment/templates/**/*.html'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
