b5a74ddaaa63cbdf487c180cd4c69d7387785c72
Délita Makanda <delita.makanda@gmail.com>
2021-01-31 00:17:12 +0100

    fix django_herooku

--- marketplace/settings_production.py
+++ marketplace/settings_production.py
@@ -1,12 +1,21 @@
 import os
 from marketplace.settings import *
 import dj_database_url
+import django_heroku
 
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = ['*', ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+django_heroku.settings(
+    locals(),
+    allowed_hosts=True,
+    secret_key=True,
+    staticfiles=False,
+    test_runner=True,
+    logging=True,
+)
