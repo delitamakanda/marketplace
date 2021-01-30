ae51df7ebedf634f46bad1664a99dc7acbfea34c
Délita Makanda <delita.makanda@gmail.com>
2021-01-31 00:32:01 +0100

    remove django_heroku

--- marketplace/settings_production.py
+++ marketplace/settings_production.py
@@ -1,7 +1,6 @@
 import os
 from marketplace.settings import *
 import dj_database_url
-import django_heroku
 
 DATABASES['default'] = dj_database_url.config()
 
@@ -10,12 +9,3 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*', ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-django_heroku.settings(
-    locals(),
-    allowed_hosts=True,
-    secret_key=True,
-    staticfiles=False,
-    test_runner=True,
-    logging=True,
-)
