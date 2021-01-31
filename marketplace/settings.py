605231d6ee79fc57407104d57573c3facf5beb0d
Délita Makanda <delita.makanda@gmail.com>
2021-01-31 22:06:34 +0100

    change storage staticfiles

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -68,7 +68,7 @@ MORE_WHITENOISE = [
 
 WHITENOISE_INDEX_FILE = True
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 ROOT_URLCONF = 'marketplace.urls'
 
