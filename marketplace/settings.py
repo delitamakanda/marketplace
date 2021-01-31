c5070280a32713ccc301049ca8b6261fc34742a1
Délita Makanda <delita.makanda@gmail.com>
2021-01-31 23:13:09 +0100

    fix settings fuck it

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'catalog.middleware.MoreWhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
